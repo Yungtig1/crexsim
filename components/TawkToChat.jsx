477d6778738070645ac6e6f797d2c9a4802dfbf0
bryanqueen <somtochukwuumehdaniel@gmail.com>
2025-02-16 19:15:17 +0100

    Update: Added Tawkto ID

--- components/TawkToChat.jsx
+++ components/TawkToChat.jsx
@@ -4,15 +4,15 @@ import { useEffect } from 'react'
 
 export default function TawkToChat() {
   useEffect(() => {
-    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
     (function(){
-      var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0]
-      s1.async = true
-      s1.src = 'https://embed.tawk.to/YOUR_TAWK_TO_ID/default' // Replace with your Tawk.to embed URL
-      s1.charset = 'UTF-8'
-      s1.setAttribute('crossorigin','*')
-      s0.parentNode.insertBefore(s1,s0)
-    })()
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/67b22a23a5c5e6190b274516/1ik7v96q9';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
   }, [])
 
   return null
